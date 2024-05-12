7f229bf051bd028adabf246d2ba35fba69b2fea6
Bidobelemti <mauriceuve3221@gmail.com>
2024-05-12 18:37:09 -0500

    Tarea finalizada

--- codigos/ejercicio7.py
+++ codigos/ejercicio7.py
@@ -1,2 +1,19 @@
-import numpy as np
+from decimal import Decimal
 
+x = 0.25
+ec_derecha = Decimal((1+2*x)/(1+x+x**(2)))
+print(ec_derecha)
+n = 1
+bandera = False
+suma = 0
+while bandera == False:
+    diferencia = 0
+    num = Decimal(n*(1*x**(2**(n-1)-1)))-Decimal((2*x**(2**(n)-1)))
+    den = Decimal(1-x**(2**(n-1))+x**(2**(n)))
+    termino = num / den
+    suma += termino
+    diferencia = abs(ec_derecha-suma)
+    n += 1
+    if diferencia < 1e-6:
+        bandera = True
+print(suma)
